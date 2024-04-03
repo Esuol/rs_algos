62fe8247c2fed2f371429d3260409843837b34d2
berlinen <1292393108@qq.com>
2024-04-03 16:23:55 +0800

    feat: bucket_Sort

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -7,8 +7,10 @@ pub fn bubble_sort<T: PartialOrd>(arr: &mut [T]) {
     }
 
     for i in 0..(size - 1) {
+        // 标记当前循环是否发生元素交换
         let mut swapped = false;
 
+        // 最后i个元素已经排好了顺序，不需要再比较
         for j in 1..(size - i) {
             if arr[j] < arr[j - 1] {
                 arr.swap(j, j - 1);
--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -1,1 +1,5 @@
 pub mod bubble_sort;
+pub mod bucket_sort;
+
+pub use bubble_sort::bubble_sort;
+pub use bucket_sort::bucket_sort;
