0306707143911a3a1c16b6566519e27023f9ba0c
berlinen <1292393108@qq.com>
2024-04-04 18:21:29 +0800

    feat: selection

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ mod is_sorted;
 mod sorting;
 
 pub use is_sorted::is_sorted;
-pub use sorting::{bubble_sort, bucket_sort};
+pub use sorting::{bubble_sort, bucket_sort, selection_sort};
--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -1,5 +1,7 @@
 pub mod bubble_sort;
 pub mod bucket_sort;
+pub mod selection_sort;
 
 pub use bubble_sort::bubble_sort;
 pub use bucket_sort::bucket_sort;
+pub use selection_sort::selection_sort;
