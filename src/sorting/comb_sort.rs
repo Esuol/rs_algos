b8bee0f6f21212f2ad1e89e4d1ecb44037ac739d
berlinen <1292393108@qq.com>
2024-04-05 17:57:09 +0800

    feat: gap sorted test

--- src/sorting/comb_sort.rs
+++ src/sorting/comb_sort.rs
@@ -1,22 +1,21 @@
 // # 梳排序
 // # 梳排序是冒泡排序的改进版本，它的思想是通过增大冒泡排序中两个相邻元素之间的距离来消除逆序对。
 
-pub fn comp_sort<T: Ord>(arr: &mut [T]) {
+pub fn comb_sort<T: Ord>(arr: &mut [T]) {
     let mut gap = arr.len();
     let shrink = 1.3;
     let mut sorted = false;
 
     while !sorted {
-        gap = (gap as f64 / shrink).floor() as usize;
-
+        gap = (gap as f32 / shrink).floor() as usize;
         if gap <= 1 {
             gap = 1;
             sorted = true;
         }
-
-        for i in 0..(arr.len() - gap) {
-            if arr[i] > arr[i + gap] {
-                arr.swap(i, i + gap);
+        for i in 0..arr.len() - gap {
+            let j = i + gap;
+            if arr[i] > arr[j] {
+                arr.swap(i, j);
                 sorted = false;
             }
         }
@@ -29,23 +28,23 @@ mod tests {
     use crate::is_sorted;
 
     #[test]
-    fn test_empty_vec() {
-        let mut empty_vec: Vec<String> = vec![];
-        comp_sort(&mut empty_vec);
-        assert_eq!(empty_vec, Vec::<String>::new());
-    }
-
-    #[test]
     fn descending() {
         let mut descending = vec![5, 4, 3, 2, 1];
-        comp_sort(&mut descending);
+        comb_sort(&mut descending);
         assert!(is_sorted(&descending));
     }
 
     #[test]
     fn ascending() {
         let mut ascending = vec![1, 2, 3, 4, 5];
-        comp_sort(&mut ascending);
+        comb_sort(&mut ascending);
         assert!(is_sorted(&ascending));
+    }
+
+    #[test]
+    fn basic() {
+        let mut basic = vec![4, 65, 2, -31, 0, 99, 2, 83, 782, 1];
+        comb_sort(&mut basic);
+        assert!(is_sorted(&basic));
     }
 }
--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -1,11 +1,11 @@
 pub mod bubble_sort;
 pub mod bucket_sort;
 pub mod cocktail_shaker_sort;
-pub mod comp_sort;
+pub mod comb_sort;
 pub mod selection_sort;
 
 pub use bubble_sort::bubble_sort;
 pub use bucket_sort::bucket_sort;
 pub use cocktail_shaker_sort::cocktail_shaker_sort;
-pub use comp_sort::comp_sort;
+pub use comb_sort::comb_sort;
 pub use selection_sort::selection_sort;
