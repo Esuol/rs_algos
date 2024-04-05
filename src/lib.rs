1c2ccf76f5fccf28dfba5374447b8caabc99669d
berlinen <1292393108@qq.com>
2024-04-05 17:53:52 +0800

    feat: gap sorted pub

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ mod is_sorted;
 mod sorting;
 
 pub use is_sorted::is_sorted;
-pub use sorting::{bubble_sort, bucket_sort, cocktail_shaker_sort, selection_sort};
+pub use sorting::{bubble_sort, bucket_sort, cocktail_shaker_sort, comp_sort, selection_sort};
