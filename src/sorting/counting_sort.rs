b1397c4e6bc1b4f1fadb8ad07c3b1900393bd712
berlinen <1292393108@qq.com>
2024-04-05 18:12:42 +0800

    feat: counting sort test

--- src/sorting/counting_sort.rs
+++ src/sorting/counting_sort.rs
@@ -22,3 +22,37 @@ pub fn counting_sort(arr: &mut [usize]) {
 
     arr.copy_from_slice(&output);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::is_sorted;
+
+    #[test]
+    fn basic() {
+        let mut basic = vec![4, 65, 2, 31, 0, 99, 2, 83, 782, 1];
+        counting_sort(&mut basic);
+        assert!(is_sorted(&basic));
+    }
+
+    #[test]
+    fn counting_sort_pre_sorted() {
+        let mut pre_sorted = vec![1, 2, 3, 4, 5];
+        counting_sort(&mut pre_sorted);
+        assert!(is_sorted(&pre_sorted));
+    }
+
+    #[test]
+    fn generic_counting_sort() {
+        let mut basic = vec![4, 65, 2, 31, 0, 99, 2, 83, 782, 1];
+        counting_sort(&mut basic);
+        assert!(is_sorted(&basic));
+    }
+
+    #[test]
+    fn presorted_u64_counting_sort() {
+        let mut pre_sorted = vec![1, 2, 3, 4, 5];
+        counting_sort(&mut pre_sorted);
+        assert!(is_sorted(&pre_sorted));
+    }
+}
