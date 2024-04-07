3779f3ce44d770ae8f0b5f3c1c0357dc21afef57
berlinen <1292393108@qq.com>
2024-04-07 11:35:24 +0800

    feat: gnome_sort

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -3,6 +3,7 @@ pub mod bucket_sort;
 pub mod cocktail_shaker_sort;
 pub mod comb_sort;
 pub mod counting_sort;
+pub mod gnome_sort;
 pub mod selection_sort;
 
 pub use bubble_sort::bubble_sort;
@@ -10,4 +11,5 @@ pub use bucket_sort::bucket_sort;
 pub use cocktail_shaker_sort::cocktail_shaker_sort;
 pub use comb_sort::comb_sort;
 pub use counting_sort::counting_sort;
+pub use gnome_sort::gnome_sort;
 pub use selection_sort::selection_sort;
