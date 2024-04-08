6c06d34340a60d27685c8cc9574183f76e8cb60a
berlinen <1292393108@qq.com>
2024-04-08 14:07:21 +0800

    fix(iam): insertion sort test

--- src/sorting/insertion_sort.rs
+++ src/sorting/insertion_sort.rs
@@ -21,3 +21,51 @@ pub fn insertion_sort_binary_search<T: Ord>(arr: &mut [T]) {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::is_sorted;
+
+    #[test]
+    fn basic() {
+        let mut basic = vec![4, 65, 2, 31, 0, 99, 2, 83, 782, 1];
+        insertion_sort(&mut basic);
+        assert!(is_sorted(&basic));
+    }
+
+    #[test]
+    fn basic_binary_search() {
+        let mut basic = vec![4, 65, 2, 31, 0, 99, 2, 83, 782, 1];
+        insertion_sort_binary_search(&mut basic);
+        assert!(is_sorted(&basic));
+    }
+
+    #[test]
+    fn empty() {
+        let mut empty: Vec<i32> = vec![];
+        insertion_sort(&mut empty);
+        assert!(empty.is_empty());
+    }
+
+    #[test]
+    fn empty_binary_search() {
+        let mut empty: Vec<i32> = vec![];
+        insertion_sort_binary_search(&mut empty);
+        assert!(empty.is_empty());
+    }
+
+    #[test]
+    fn already_sorted() {
+        let mut sorted = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        insertion_sort(&mut sorted);
+        assert!(is_sorted(&sorted));
+    }
+
+    #[test]
+    fn already_sorted_binary_search() {
+        let mut sorted = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        insertion_sort_binary_search(&mut sorted);
+        assert!(is_sorted(&sorted));
+    }
+}
--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -4,14 +4,6 @@ pub mod cocktail_shaker_sort;
 pub mod comb_sort;
 pub mod counting_sort;
 pub mod gnome_sort;
-pub mod selection_sort;
 pub mod heap_sort;
-
-pub use bubble_sort::bubble_sort;
-pub use bucket_sort::bucket_sort;
-pub use cocktail_shaker_sort::cocktail_shaker_sort;
-pub use comb_sort::comb_sort;
-pub use counting_sort::counting_sort;
-pub use gnome_sort::gnome_sort;
-pub use selection_sort::selection_sort;
-pub use heap_sort::heap_sort;
+pub mod insertion_sort;
+pub mod selection_sort;
