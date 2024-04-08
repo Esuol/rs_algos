9aca1498fbb7be473c8fbd71f30938c50f3575cb
berlinen <1292393108@qq.com>
2024-04-08 11:04:19 +0800

    fix(iam): 堆排序

--- src/sorting/heap_sort.rs
+++ src/sorting/heap_sort.rs
@@ -1,21 +1,37 @@
-// 堆排序
+// 堆排
+pub fn heap_sort<T: PartialOrd>(arr: &mut [T]) {
+    let len = arr.len();
+
+    // 构建大根堆
+    for i in (0..len / 2).rev() {
+        heapify(arr, i, len);
+    }
+
+    // 每轮循环将堆顶元素（也就是最大元素）放到最后
+    for i in (1..len).rev() {
+        arr.swap(0, i);
+        // 恢复大根堆
+        heapify(arr, 0, i);
+    }
+}
+
 fn heapify<T: PartialOrd>(arr: &mut [T], root: usize, end: usize) {
-    let mut laggest = root;
+    let mut largest = root;
 
     let left = 2 * root + 1;
 
-    if left < end && arr[left] > arr[laggest] {
-        laggest = left;
+    if left < end && arr[left] > arr[largest] {
+        largest = left;
     }
 
     let right = 2 * root + 2;
 
-    if right < end && arr[right] > arr[laggest] {
-        laggest = right;
+    if right < end && arr[right] > arr[largest] {
+        largest = right;
     }
 
-    if laggest != root {
-        arr.swap(root, laggest);
-        heapify(arr, laggest, end);
+    if largest != root {
+        arr.swap(root, largest);
+        heapify(arr, largest, end);
     }
 }
