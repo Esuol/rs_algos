996ee8b4d00bdf49259fec53254e70149fc79fac
berlinen <1292393108@qq.com>
2024-04-10 11:00:49 +0800

    feat: merge sort

---
+++ src/sorting/merge_sort.rs
@@ -0,0 +1,52 @@
+// 归并排序
+pub fn merge_sort<T>(arr: &mut [T])
+where
+    T: PartialOrd + Clone + Default,
+{
+    if (arr.len() > 1) {
+        merge_sort_range(arr, 0, arr.len() - 1);
+    }
+}
+
+fn merge_sort_range<T>(arr: &mut [T], start: usize, end: usize)
+where
+    T: PartialOrd + Clone + Default,
+{
+    if start < end {
+        let mid = (start + end) / 2;
+        merge_sort_range(arr, start, mid);
+        merge_sort_range(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+}
+
+fn merge<T>(arr: &mut [T], start: usize, mid: usize, end: usize)
+where
+    T: PartialOrd + Clone + Default,
+{
+    let mut left = arr[start..=mid].to_vec();
+    let mut right = arr[mid + 1..=end].to_vec();
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = start;
+    while i < left.len() && j < right.len() {
+        if left[i] <= right[j] {
+            arr[k] = left[i].clone();
+            i += 1;
+        } else {
+            arr[k] = right[j].clone();
+            j += 1;
+        }
+        k += 1;
+    }
+    while i < left.len() {
+        arr[k] = left[i].clone();
+        i += 1;
+        k += 1;
+    }
+    while j < right.len() {
+        arr[k] = right[j].clone();
+        j += 1;
+        k += 1;
+    }
+}
